4537ec879b7a803ebd74e825b8cbcc5c1a585278
naresh1407 <nareshkumarmakwana1407@gmail.com>
2021-08-08 13:48:00 +0530

    first version of the bird Gallery website

---
+++ script.js
@@ -0,0 +1,20 @@
+const frames = document.querySelectorAll('.panel'); 
+
+frames.forEach(currFrame => {
+    currFrame.addEventListener('mouseenter', () => {
+        removeActive();
+        currFrame.classList.add('active');
+    });
+});
+
+frames.forEach(currFrame => {
+    currFrame.addEventListener('mouseover', () => {
+        currFrame.classList.remove('active'); 
+    })
+}); 
+
+function removeActive() {
+    frames.forEach(currFrame => {
+        currFrame.classList.remove('active');
+    });
+}
